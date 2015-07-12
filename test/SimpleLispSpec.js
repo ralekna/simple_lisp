a541540ab661d675685b20250a100ba5da087640
Rytis Alekna <r.alekna@gmail.com>
2015-07-12 22:31:33 +0300

    direct eval in progress

--- test/SimpleLispSpec.js
+++ test/SimpleLispSpec.js
@@ -1,19 +1,43 @@
 /**
  * Created by rytis on 06/07/15.
  */
-import { evalAST } from 'lib/SimpleLisp';
+import { evalAST, evalEntities, OPERATORS } from 'lib/SimpleLisp';
 
 describe('Simple lisp', () => {
-  it('should provide AST evaluation functionality', () => {
-    expect(evalAST(['quote', [true, false]])).toEqual([true, false]);
-  });
+ /*
+  describe('single sentence function calls', () => {
 
-  it('should return sum', () => {
-    expect(evalAST(['+', 2, 3])).toEqual(5);
-  });
+    it('should provide AST evaluation functionality', () => {
+      expect(evalAST(['QUOTE', [true, false]])).toEqual([true, false]);
+    });
 
-  it('should return cascaded sum', () => {
-    expect(evalAST(['+', 2, 3, ['+', 5, 3, 3]])).toEqual(16);
+    it('should return sum', () => {
+      expect(evalAST(['PLUS', 2, 3])).toEqual(5);
+    });
+
+    it('should return cascaded sum', () => {
+      expect(evalAST(['PLUS', 2, 3, ['PLUS', 5, 3, 3]])).toEqual(16);
+    });
+
+  })
+*/
+  describe('multi sentence evaluator', () => {
+
+    var program = [
+      [OPERATORS.PLUS, 2, 3],
+      [OPERATORS.PLUS, 4, 3]
+    ];
+
+    it('should eval plain function', () => {
+      expect(evalEntities(OPERATORS.PLUS, 2, 3)).toEqual(5);
+    });
+
+
+    it('should return last result', () => {
+      console.log('in test')
+      expect(evalEntities(program).toEqual(7));
+    });
+
   });
 
 });
